Vman45/tutanota

commit 4009da8fe5024ff6a82d672abaab2c97733179a9

Fix list border being displayed in mobile layout, fix #1623

src/gui/base/List.js
@@ -19,6 +19,7 @@ import {windowFacade} from "../../misc/WindowFacade"
 import {BadRequestError} from "../../api/common/error/RestError"
 import {SwipeHandler} from "./SwipeHandler"
 import {applySafeAreaInsetMarginLR} from "../HtmlUtils"
+import {styles} from "../styles"
 
 assertMainOrNode()
 
@@ -150,7 +151,8 @@ export class List<T: ListElement, R:VirtualRow<T>> {
 			.setVisible(false)
 
 		this.view = (): VirtualElement => {
-			let list = m(".list-container[tabindex=-1].fill-absolute.scroll.list-border-right.list-bg.nofocus.overflow-x-hidden", {
+			let list = m(".list-container[tabindex=-1].fill-absolute.scroll.list-bg.nofocus.overflow-x-hidden"
+				+ (styles.isDesktopLayout() ? ".list-border-right" : ""), {
 				oncreate: (vnode) => {
 					this._domListContainer = vnode.dom
 					this._width = this._domListContainer.clientWidth

commit 0826aa6cd459058a77e1796f7a734ea0f7971735

Fix not displaying list border in 2-column mobile layout
4009da8f introduced a bug. Border is moved to the ViewSlider from the
list and folder column now.

src/gui/base/List.js
@@ -19,7 +19,6 @@ import {windowFacade} from "../../misc/WindowFacade"
 import {BadRequestError} from "../../api/common/error/RestError"
 import {SwipeHandler} from "./SwipeHandler"
 import {applySafeAreaInsetMarginLR} from "../HtmlUtils"
-import {styles} from "../styles"
 import {theme} from "../theme"
 
 assertMainOrNode()
@@ -149,8 +148,7 @@ export class List<T: ListElement, R:VirtualRow<T>> {
 
 
 		this.view = (): VirtualElement => {
-			let list = m(".list-container[tabindex=-1].fill-absolute.scroll.list-bg.nofocus.overflow-x-hidden"
-				+ (styles.isDesktopLayout() ? ".list-border-right" : ""), {
+			let list = m(".list-container[tabindex=-1].fill-absolute.scroll.list-bg.nofocus.overflow-x-hidden", {
 				oncreate: (vnode) => {
 					this._domListContainer = vnode.dom
 					this._width = this._domListContainer.clientWidth

src/gui/base/ViewColumn.js
@@ -11,6 +11,8 @@ export const ColumnType = {
 	Foreground: 0
 }
 
+type Attrs = {rightBorder?: boolean}
+
 export class ViewColumn {
 	component: Component;
 	columnType: ColumnTypeEnum;
@@ -43,9 +45,10 @@ export class ViewColumn {
 		this.isInForeground = false
 		this.visible = false
 
-		this.view = () => {
+		this.view = (vnode: Vnode<Attrs>) => {
 			let zIndex = !this.visible && this.columnType === ColumnType.Foreground ? ".z4" : ""
-			return m(".view-column..overflow-x-hidden.fill-absolute.backface_fix" + zIndex, {
+			const border = vnode.attrs.rightBorder ? ".list-border-right" : ""
+			return m(".view-column.overflow-x-hidden.fill-absolute.backface_fix" + zIndex + border, {
 					"aria-hidden": this.visible ? "false" : "true",
 					oncreate: (vnode) => {
 						this._domColumn = vnode.dom

src/gui/base/ViewSlider.js
@@ -69,6 +69,8 @@ export class ViewSlider implements IViewSlider {
 		this._isModalBackgroundVisible = false
 
 		this.view = (): Children => {
+			const mainSliderColumns = this._getColumnsForMainSlider()
+			const allBackgroundColumnsAreVisible = this._visibleBackgroundColumns.length === mainSliderColumns.length
 			return m(".fill-absolute.flex.col", {
 				oncreate: (vnode) => {
 					this._attachTouchHandler(vnode.dom)
@@ -89,7 +91,11 @@ export class ViewSlider implements IViewSlider {
 							width: this.getWidth() + 'px',
 							transform: 'translateX(' + this.getOffset(this._visibleBackgroundColumns[0]) + 'px)',
 						}
-					}, this._getColumnsForMainSlider().map(m)
+					}, mainSliderColumns.map((column, index) => m(column, {
+						// Only apply right border if 1. all background columns are visible. 2. It's not the last column.
+						// Perhaps the condition should be "there's another visible column after this one" but it works like this too
+						rightBorder: allBackgroundColumnsAreVisible && index !== mainSliderColumns.length - 1
+					}))
 				),
 				styles.isUsingBottomNavigation() ? m(BottomNav) : null,
 				this._getColumnsForOverlay().map(m),

src/gui/main-styles.js
@@ -606,7 +606,6 @@ styles.registerStyle('main', () => {
 		' .folder-column': {
 			'background-color': theme.navigation_bg,
 			height: '100%',
-			'border-right': `1px solid ${theme.navigation_border}`
 		},
 		'.list-border-right': {
 			'border-right': `1px solid ${theme.list_border}`